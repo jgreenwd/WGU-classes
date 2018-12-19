7f9f9c5780db3a756d5ed972a6208d8781c8ac6d
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2018-12-19 15:19:51 -0600

    Update student.cpp

--- C867/student.cpp
+++ C867/student.cpp
@@ -8,17 +8,22 @@
 
 #include "student.h"
 #include "degree.h"
-using std::string;
 
-Student(std::string student_ID = "ID", std::string lname = "last name", std::string fname = "first name",
-        std::string email = "student@school.edu", Degree degree = SOFTWARE, int age = 18, int[] = [1460]) {
-};
+Student::Student(std::string ID,
+                 std::string fname,
+                 std::string lname,
+                 std::string email,
+                 Degree deg,
+                 int Age,
+                 int days[])
+: student_ID(ID), first_name(fname), last_name(lname), email_addr(email), degree(deg), age(Age), days_remaining(days)
+{};
 
-~Student(){
+Student::~Student() {
     
 };
 
-std::string Student::get_ID(void) {
+std::string Student::get_ID() {
     return student_ID;
 };
 
@@ -26,7 +31,7 @@ void Student::set_ID(std::string ID) {
     student_ID = ID;
 };
 
-std::string Student::get_lname(void){
+std::string Student::get_lname(){
     return last_name;
 };
 
@@ -34,7 +39,7 @@ void Student::set_lname(std::string lname){
     last_name = lname;
 };
 
-std::string Student::get_fname(void) {
+std::string Student::get_fname() {
     return first_name;
 };
 
@@ -42,7 +47,7 @@ void Student::set_fname(std::string fname) {
     first_name = fname;
 };
 
-std::string Student::get_email(void) {
+std::string Student::get_email() {
     return email_addr;
 };
 
@@ -50,7 +55,7 @@ void Student::set_email(std::string email) {
     email_addr = email;
 };
 
-Degree Student::get_Degree(void) {
+Degree Student::get_Degree() {
     return degree;
 };
 
@@ -58,7 +63,7 @@ void Student::set_Degree(Degree deg) {
     degree = deg;
 };
 
-int Student::get_age(void) {
+int Student::get_age() {
     return age;
 };
 
@@ -66,11 +71,11 @@ void Student::set_age(int Age) {
     age = Age;
 };
 
-int Student::get_days_rem(void) {
+int Student::get_days_rem() {
     return *days_remaining;
 };
 
-void Student::set_days_rem(int[] days) {
-    days_remaining = *days;
+void Student::set_days_rem(int* days) {
+    *days_remaining = *days;
 };
 
