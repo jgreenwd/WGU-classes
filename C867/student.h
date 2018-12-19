7f90bbc65b69d23dae49c8320cdac5af924a82dc
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2018-12-19 15:19:19 -0600

    Update student.h

--- C867/student.h
+++ C867/student.h
@@ -6,12 +6,11 @@
 //  Copyright © 2018 Jeremy Greenwood. All rights reserved.
 //
 
-#ifndef student_hpp
-#define student_hpp
+#ifndef student_h
+#define student_h
 
 #include <iostream>
 #include "degree.h"
-using std::string;
 
 class Student {
     private:
@@ -20,25 +19,25 @@ class Student {
                     last_name,
                     email_addr;
         Degree      degree;
-        int         age,
-                    days_remaining[];
+        int         age;
+        int*        days_remaining;
     public:
-        Student(std::string, std::string, std::string, std::string, Degree, int, int[]);
+        Student(std::string, std::string, std::string, std::string, Degree, int, int*);
         ~Student();
-        std::string Student::get_ID(void);
-        void Student::set_ID(std::string);
-        std::string Student::get_lname(void);
-        void Student::set_lname(std::string);
-        std::string Student::get_fname(void);
-        void Student::set_fname(std::string);
-        std::string Student::get_email(void);
-        void Student::set_email(std::string);
-        Degree Student::get_Degree(void);
-        void Student::set_Degree(Degree);
-        int Student::get_age(void);
-        void Student::set_age(int);
-        int[] Student::get_days_rem(void);
-        void Student::set_days_rem(int[]);
-}
+        std::string get_ID(void);
+        void set_ID(std::string);
+        std::string get_lname(void);
+        void set_lname(std::string);
+        std::string get_fname(void);
+        void set_fname(std::string);
+        std::string get_email(void);
+        void set_email(std::string);
+        Degree get_Degree(void);
+        void set_Degree(Degree);
+        int get_age(void);
+        void set_age(int);
+        int get_days_rem(void);
+        void set_days_rem(int*);
+};
 
-#endif /* student_hpp */
+#endif /* student_h */
