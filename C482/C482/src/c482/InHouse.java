7fac8c01db1222dcd182f95733775f48c8ec9d30
Jeremy Greenwood <jgreenwd@hotmail.com>
2019-02-17 15:15:02 -0600

    feat: add InHouse child class

---
+++ C482/C482/src/c482/InHouse.java
@@ -0,0 +1,19 @@
+/**
+ * author Jeremy Greenwood
+ * mentor Rebekah Coggin
+ * WGU-ID 000917613
+ * course C482
+ */
+
+package c482;
+
+public class InHouse extends Part {
+    private int machineID;
+    
+    public InHouse(int PartID, String name, double price, int inStock, int min, int max) {
+        super(PartID, name, price, inStock, min, max);
+    }
+    
+    public int getMachineID() { return machineID; };
+    public void setMachineID(int ID) { this.machineID = ID; }
+}
