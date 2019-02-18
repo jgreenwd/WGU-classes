de1717e243031970c71d51136b85e72d5640a410
Jeremy Greenwood <jgreenwd@hotmail.com>
2019-02-17 19:05:19 -0600

    feat: add part/product-screen controller templates

---
+++ C482/C482/src/c482/PartScreenController.java
@@ -0,0 +1,20 @@
+/**
+ * author Jeremy Greenwood
+ * mentor Rebekah Coggin
+ * WGU-ID 000917613
+ * course C482
+ */
+package c482;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class PartScreenController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
