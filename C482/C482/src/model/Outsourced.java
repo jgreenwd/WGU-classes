556a4080961d3f594d4fc34a47dcb85e79a2708a
Jeremy Greenwood <jgreenwd@hotmail.com>
2019-03-11 18:51:16 -0500

    refactor: use constructor chaining in Part & derived classes

--- C482/C482/src/model/Outsourced.java
+++ C482/C482/src/model/Outsourced.java
@@ -10,13 +10,16 @@ package model;
 public final class Outsourced extends Part {
     private String companyName;
     
-    public Outsourced() {
-        super();
+    // Outsourced with UNknown PartID
+    public Outsourced(String name, double price, int inStock, int min, int max, String source) {
+        super(name, price, inStock, min, max);
+        setCompanyName(source);
     }
     
-    public Outsourced(int PartID, String name, double price, int inStock, int min, int max, String companyName) {
-        super(PartID, name, price, inStock, min, max);
-        setCompanyName(companyName);
+    // Outsourced with KNOWN PartID
+    public Outsourced(int PartID, String name, double price, int inStock, int min, int max, String source) {
+        this(name, price, inStock, min, max, source);
+        this.setPartID(PartID);
     }
     
     public String getCompanyName() { return companyName; }
