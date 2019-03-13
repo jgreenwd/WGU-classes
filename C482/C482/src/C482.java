16a7433b597f17e0c011ffa74c8e23fc3767f88f
Jeremy Greenwood <jgreenwd@hotmail.com>
2019-03-13 13:50:47 -0500

    feat: full set of dummy-data

--- C482/C482/src/C482.java
+++ C482/C482/src/C482.java
@@ -8,6 +8,7 @@
 import model.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,7 +17,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 public class C482 extends Application {
-    
     private Stage primaryStage;
     private BorderPane rootLayout;
     
@@ -51,74 +51,69 @@ public class C482 extends Application {
             e.printStackTrace();
         }
     }
-    
 
     public Stage getPrimaryStage() {
         return primaryStage;
     }
     
 
-    /* ---------- Development Test Data ---------- */
+    /* ---------- Fake Development Data ---------- */
     public static void loadDB() {
-        InHouse part1 = new InHouse(001,"Widget",1.99, 0,0,999,1);
-        InHouse part2 = new InHouse(002,"Wedget",2.95, 0,0,999,2);
-        Outsourced part3 = new Outsourced(003,"Wodget",2.99, 0,0,999,"ACME");
-        Outsourced part4 = new Outsourced(004,"Wudget", 1.96, 0,0,999,"Ajax");
+        // add parts
+        InHouse part1 = new InHouse(1,"Widget",1.99, 100,10,500,123123);
+        InHouse part2 = new InHouse(2,"Wadget",2.95, 95,50,100,124124);
+        InHouse part3 = new InHouse(3,"Wedget",0.57, 250,100,1000,120125);
+        Outsourced part4 = new Outsourced(4,"Wodget", 2.99, 107,25,250,"ACME");
+        Outsourced part5 = new Outsourced(5,"Wudget", 1.96, 130,25,250,"Ajax");
+        Outsourced part6 = new Outsourced(6,"Wydget", 4.97, 20,50,10,"StuffCo");
+        InHouse part7 = new InHouse(7,"Gadget",1.90, 100,10,500,120125);
+        InHouse part8 = new InHouse(8,"Gizmo",1.93, 87,25,250,337345);
+        InHouse part9 = new InHouse(9,"Gubbins",1.97, 43,15,100,212657);
+        Outsourced part10 = new Outsourced(10,"Thingy", 2.99, 19,50,10,"ThePlace");
+        Outsourced part11 = new Outsourced(11,"Thingamajig", 1.96, 12,50,10,"ACME");
+        Outsourced part12 = new Outsourced(12,"Thingamabob", 1.92, 45,50,10,"Bob's Things");
         
         Inventory.addPart(part1);
         Inventory.addPart(part2);
         Inventory.addPart(part3);
         Inventory.addPart(part4);
+        Inventory.addPart(part5);
+        Inventory.addPart(part6);
+        Inventory.addPart(part7);
+        Inventory.addPart(part8);
+        Inventory.addPart(part9);
+        Inventory.addPart(part10);
+        Inventory.addPart(part11);
+        Inventory.addPart(part12);
         
         // add products
-        ArrayList<Part> partset1 = new ArrayList<>();
-        partset1.add(part1);
-        partset1.add(part2);
-        partset1.add(part3);
-        partset1.add(part4);
+        ArrayList<Part> partset1 = new ArrayList<>( Arrays.asList( new Part[]{part1, part2, part3} ) );
+        ArrayList<Part> partset2 = new ArrayList<>( Arrays.asList( new Part[]{part2, part1, part4} ) );
+        ArrayList<Part> partset3 = new ArrayList<>( Arrays.asList( new Part[]{part3, part12, part5} ) );
+        ArrayList<Part> partset4 = new ArrayList<>( Arrays.asList( new Part[]{part4, part11, part6} ) );
+        ArrayList<Part> partset5 = new ArrayList<>( Arrays.asList( new Part[]{part5, part10, part7} ) );
+        ArrayList<Part> partset6 = new ArrayList<>( Arrays.asList( new Part[]{part6, part9, part8} ) );
+        ArrayList<Part> partset7 = new ArrayList<>( Arrays.asList( new Part[]{part7, part8, part9} ) );
         
-        ArrayList<Part> partset2 = new ArrayList<>();
-        partset2.add(part2);
-        partset2.add(part4);
-        
-        Product product1 = new Product(partset1, 001, "Doohickey", 14.95, 0,0,999);
-        Product product2 = new Product(partset2, 002, "Doodad", 7.81, 0,0,999);
+        Product product1 = new Product(partset1, 1, "Doohickey", 14.95, 10,2,20);
+        Product product2 = new Product(partset2, 2, "Doodad", 19.85, 25,10,100);
+        Product product3 = new Product(partset3, 3, "Whatsit", 11.15, 10,2,20);
+        Product product4 = new Product(partset4, 4, "Whatchamacallit", 24.75, 25,10,100);
+        Product product5 = new Product(partset5, 5, "Appliance", 17.25, 25,10,100);
+        Product product6 = new Product(partset6, 6, "Contraption", 22.15, 25,10,100);
+        Product product7 = new Product(partset7, 7, "Device", 14.50, 25,10,100);
         
         Inventory.addProduct(product1);
         Inventory.addProduct(product2);
+        Inventory.addProduct(product3);
+        Inventory.addProduct(product4);
+        Inventory.addProduct(product5);
+        Inventory.addProduct(product6);
+        Inventory.addProduct(product7);
     }
     
     public static void main(String[] args) {
         loadDB();
         launch(args);
     }
-    
-    /* ---------- synonyms for widget: ----------
-        001,"Widget",1.99, 0,0,999
-        002,"Wodget",2.95, 0,0,999
-        003,"Wudget",3.90, 0,0,999
-    
-        004,"Thingy",2.99, 0,0,999
-        005,"Thingamajig", 1.96, 0,0,999
-        006,"Thingamabob", 1.92, 0,0,999
-    
-        007,"Doohickey",0.94, 0,0,999
-        008,"Doodad", 1.91, 0,0,999
-        009,"Diddlydilly", 2.97, 0,0,999
-    
-        010,"Whatsit",0.10, 0,0,999
-        011,"Whatchamacallit", 1.95, 0,0,999
-        012,"Whatsis", 1.98, 0,0,999
-    
-        013,"Gadget", 1.90, 0,0,999
-        014,"Gizmo", 1.93, 0,0,999
-        015,"Gubbins", 1.97, 0,0,999
-        
-
-        012, "stuff", 1.94, 0,0,999
-        
-        017, "appliance", 1.89, 0,0,999
-        018, "contraption", 1.88, 0,0,999
-        020, "device", 1.86, 0,0,999
-    */
 }
