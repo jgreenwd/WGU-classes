731556cc496cc5e563a36ec7de2bf097105a6236
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-10-28 15:39:26 -0500

    feat: add OUT_FOR_DELIVERY status

--- C950/package.py
+++ C950/package.py
@@ -9,9 +9,8 @@ from enum import Enum
 
 
 class Package:
-    def __init__(self, id=None, address=None, deadline=None, weight=None, notes=None):
+    def __init__(self, id=None, deadline=None, weight=None, notes=None):
         self.ID = id
-        self.address = address
         self.weight = weight
         self.notes = notes
         self.deadline = deadline
@@ -21,15 +20,15 @@ class Package:
         return ("ID: " + self.ID +
                 "\tWeight: " + "{:4.1f}".format(self.weight) +
                 "\tDelivery By: " + self.deadline.strftime("%H:%M") +
-                "\tStatus: " + self.status.name +
-                "\t\tAddress: " + str(self.address))
+                "\tStatus: " + self.status.name)
 
     def __lt__(self, other):
         return self.deadline < other.deadline
 
 
 class Status(Enum):
-    LOADING = 0
-    IN_ROUTE = 1
+    IN_ROUTE = 0
+    LOADING = 1
     DELAYED = 2
-    DELIVERED = 3
+    OUT_FOR_DELIVERY = 3
+    DELIVERED = 4
