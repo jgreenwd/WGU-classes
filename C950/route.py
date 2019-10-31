f3cc3d5f88b89ef7a58098d95b20db9f0a72d610
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-10-31 18:19:11 -0500

    feat: add create_cycle & subprocesses

--- C950/route.py
+++ C950/route.py
@@ -5,21 +5,37 @@
 # Mentor: Rebekah McBride
 
 from graph import Graph
-from destination import Destination
 
 
 class Route(Graph):
     def __init__(self):
         Graph.__init__(self)
+        self.order = []
 
-    def find_midpoint(self, start):
-        loc = start
-        max_dist = loc.get_weight(start)
+    def _find_midpoint(self, start, vert_list):
+        # return list member farthest from starting vertex
+        return sorted(vert_list, key=lambda i: i.get_weight(start))[len(vert_list) - 1]
 
-        for item in self.vertices:
-            if item is not start:
-                if item.get_weight(start) > max_dist:
-                    max_dist = item.get_weight(start)
-                    loc = item
+    def _find_next(self, current, vert_list):
+        # return list member closest to current vertex
+        return sorted(vert_list, key=lambda i: i.get_weight(current))[1]
 
-        return loc
+    def create_cycle(self, start):
+        self.order = [None] * len(self.vertices)
+        self.order[0] = start
+        tmp = sorted(list(self.vertices), key=lambda i: i.get_weight(start))
+        current = tmp[0]
+
+        i = 1
+        m = len(self.order) // 2
+        while None in self.order:
+            prev = current
+
+            if i == len(self.order) // 2:
+                self.order[i] = self._find_midpoint(start, sorted(list(self.vertices), key=lambda i: i.get_weight(start)))
+            else:
+                self.order[i] = self._find_next(current, tmp)
+
+            current = self.order[i]
+            tmp.remove(prev)
+            i += 1
