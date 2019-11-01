678f50051cfbebebb391452d7014befaf55a37a4
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-11-01 12:38:55 -0500

    docs: add docstrings to Vertex

--- C950/vertex.py
+++ C950/vertex.py
@@ -14,10 +14,13 @@ class Vertex:
         return str(self.id)
 
     def add_edge(self, neighbor, weight=0):
+        """ Add weighted edge from self to neighbor """
         self.adjacent[neighbor] = weight
 
     def get_connections(self):
+        """ Return dict keys of all connections to this Vertex """
         return self.adjacent.keys()
 
     def get_weight(self, neighbor):
+        """ Return weight of edge from self to neighbor as float """
         return self.adjacent[neighbor]
