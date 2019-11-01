3877bae7e740c2b46b51a84edb41e9eae206dbbe
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-11-01 13:06:42 -0500

    docs: update docstrings

--- C950/utility_functions.py
+++ C950/utility_functions.py
@@ -6,7 +6,7 @@ from destination import Destination
 
 
 def load_file(filename):
-    """ Read data from file and return as list. """
+    """ Read data from text file and return as list. """
     fin = open(filename)
     result = []
     if fin.readable():
@@ -30,18 +30,26 @@ def convert_time(args):
 
 
 def build_package_table(source):
-    """ Return HashTable() from List() of string input """
+    """ Given List() of string input, Return HashTable() of Package()s. """
     table = HashTable(64)
 
     for line in source:
         # separate items on each line of CSV file
         temp = line.split(',')
 
+        loc = Destination(
+            temp[1],                        # - address
+            temp[2],                        # - city
+            temp[3],                        # - state
+            temp[4]                         # - zip
+        )
+
         p = Package(
-            temp[0],                          # ID
-            convert_time((temp[5])),          # Delivery Deadline
-            float(temp[6]),                   # Weight
-            temp[7])                          # Notes
+            temp[0],                        # ID
+            loc,                            # Location
+            convert_time((temp[5])),        # Delivery Deadline
+            float(temp[6]),                 # Weight
+            temp[7])                        # Notes
 
         # add Package to HashTable
         table.insert(p)
@@ -49,8 +57,11 @@ def build_package_table(source):
     return table
 
 
+# Creates duplication of effort from build_package_table(), (parsing the same data twice)
+# but allows separation of Graph() creation from Package() creation. Implementation
+# becomes overly cumbersome when they are combined.
 def build_destination_graph(source):
-    """ Return Graph() from List() of string input """
+    """ Return Graph() of Destination()s from List() of string input. """
     locales = Graph()
 
     for line in source:
