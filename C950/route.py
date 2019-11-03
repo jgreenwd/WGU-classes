c097a94d919fa09f4d260ae53b9c6874b28d26e7
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-11-02 19:06:55 -0500

    feat: add node accessors & time tracking

--- C950/route.py
+++ C950/route.py
@@ -5,6 +5,7 @@
 # Mentor: Rebekah McBride
 
 from graph import Graph
+from datetime import time
 
 
 class Route(Graph):
@@ -12,6 +13,10 @@ class Route(Graph):
         """ Create Route Object. """
         Graph.__init__(self)
         self.order = []
+        self._start_time = time()
+        self.delivery_time = time()
+        self._rate_of_travel = 0
+        self._index = 0
 
     def _find_midpoint(self, start, vert_list):
         # return list member farthest from starting vertex
@@ -33,8 +38,6 @@ class Route(Graph):
 
         # cast Set() of vertices as List() and order by distance/weight from starting point
         tmp = sorted(list(self.vertices), key=lambda i: i.get_weight(start))
-        current = tmp[0]
-        i = 1
 
         # ALGORITHM EXPLAINED:
         # 1. Find the Destination farthest from the starting vertex
@@ -53,6 +56,8 @@ class Route(Graph):
         # 9. Repeat until no vertices remain in sorted list
         # 10. Return to starting vertex
 
+        current = tmp[0]
+        i = 1
         while None in self.order:
             # save reference to the last place visited in tmp List() to be able to .remove() it later
             prev = current
@@ -65,6 +70,39 @@ class Route(Graph):
             current = self.order[i]
             tmp.remove(prev)
             i += 1
-        
+
         # return to starting vertex
         self.order.append(start)
+
+    def set_rate_of_travel(self, rate):
+        """ Set the average speed of delivery vehicle in MPH.
+
+        :param rate: float of the vehicle speed """
+        self._rate_of_travel = rate
+
+    def set_start_time(self, time):
+        """ Set the time at which the vehicle will leave the HUB.
+
+        :param time: expects datetime() value
+        """
+        self._start_time = time
+
+    def get_start_time(self):
+        """ Return time when the delivery vehicle leaves the HUB. """
+        return self._start_time
+
+    def get_current_node(self):
+        """ Return most recent node visited in Cycle. """
+        return self.order[self._index]
+
+    def get_next_node(self):
+        """ Return next node to be visited in Cycle.
+
+            CAUTION: This method advances the internal index to the next node. Every
+            invocation of the method will return a different value.
+        """
+        if self._index < len(self.order) - 1:
+            self._index += 1
+            return self.order[self._index]
+        else:
+            return self.order[len(self.order) - 1]
