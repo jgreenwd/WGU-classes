1b33f457192b973e7da319b5d4f73e2689383691
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-11-02 22:02:39 -0500

    feat: add current_delivery_status function

--- C950/utility_functions.py
+++ C950/utility_functions.py
@@ -22,11 +22,12 @@ def convert_time(args):
     """ Given a string in "HH:MM" format, return an equivalent time object. """
     # NOTE 1: ignores AM/PM
     # NOTE 2: "End Of Day" defaults to 20:00
-    if args != "EOD":
-        result = datetime.strptime(args[:-3], "%H:%M").time()
+    if args != "EOD" and len(args) < 6:
+        return datetime.strptime(args, "%H:%M")
+    elif args == "EOD":
+        return time(hour=20, minute=00)
     else:
-        result = time(hour=20, minute=00)
-    return result
+        return datetime.strptime(args[:-3], "%H:%M").time()
 
 
 def build_package_table(source):
@@ -77,3 +78,24 @@ def build_destination_graph(source):
         locales.add_vertex(loc)
 
     return locales
+
+
+def current_delivery_status(table):
+    """ Display Package() info for all packages.
+
+    :param table: HashTable of Packages()
+    """
+    print("ID\tWgt \tAddress\t\t\t\t  City\t\t\t\t Zip\tDeliver-By\tStatus")
+    for bucket in table:
+        if len(bucket) > 0:
+            for package in bucket:
+                print("{:2} {:4.1f} \t{:20.20}  {:18.16} {}\t{}\t{:24}".format(
+                    package.ID,
+                    package.weight,
+                    package.address.address,
+                    package.address.city,
+                    package.address.zip,
+                    package.deadline,
+                    package.get_status()
+                ))
+    print()
