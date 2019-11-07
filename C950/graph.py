bfdb6303d410ec19f581af3b244018288b5b04e0
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-11-07 13:13:25 -0600

    refactor: extract Edge to its own class
    add: _get_nearest_neighbor(), _get_neighbors
    generate_edges more concise

--- C950/graph.py
+++ C950/graph.py
@@ -4,27 +4,29 @@
 # Student ID#: 000917613
 # Mentor: Rebekah McBride
 
+from edge import Edge
+
 
 class Graph:
     def __init__(self):
         """ Create Graph Object. """
         self._index = 0
-        self.vertices = set()
-        self.indices = []
-        self.weights = []
+        self._vertices = set()
         self._adjacency_list = []
+        self._indices = []
+        self._weights = []
 
     def __contains__(self, vertex):
-        return vertex in self.vertices
+        return vertex in self._vertices
 
     def __len__(self):
-        return len(self.vertices)
+        return len(self._vertices)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        iterable = list(self.vertices)
+        iterable = list(self._vertices)
         if self._index < len(iterable):
             result = iterable[self._index]
             self._index += 1
@@ -32,20 +34,25 @@ class Graph:
         self._index = 0
         raise StopIteration
 
-    def _get_weight(self, vert1, vert2):
-        """ Return weight of edge between both vertices as float. """
-        low, high = sorted([vert1, vert2])
-        return self.weights[high][low]
-
     def _get_index(self, location):
-        """ Return index of location in list or -1. """
+        """ Return index of location in list or -1.
+
+        :param location: Vertex object """
         index = 0
         search = location.address + " " + location.zip
-        for item in self.indices:
+        for item in self._indices:
             if search == item:
                 return index
             index += 1
         return -1
+
+    def _get_weight(self, vert1, vert2):
+        """ Return weight of edge between both vertex indices as float.
+
+        :param vert1: index of 1st vertex
+        :param vert2: index of 2nd vertex"""
+        low, high = sorted([vert1, vert2])
+        return self._weights[high][low]
 
     def _add_edge(self, vert1, vert2):
         """ Add an edge to the Graph() between vert1 and vert2.
@@ -53,30 +60,71 @@ class Graph:
         :param vert1: Vertex to be joined...
         :param vert2: ... Vertex joined to
         """
-        # find indexes for both vertices in order to...
+        # find indexes for both vertices
         index_1 = self._get_index(vert1)
         index_2 = self._get_index(vert2)
-        # ...find the weight for this edge
+
+        # do not add self-loops
+        if index_1 == index_2:
+            return
+
+        # find the weight for this edge
         weight = self._get_weight(index_1, index_2)
         edge = Edge(vert1, vert2, weight)
 
-        # pass reference to adjacency list
-        vert1.add_edge(vert2, weight)
-        vert2.add_edge(vert1, weight)
+        if edge not in self._adjacency_list:
+            self._adjacency_list.append(edge)
 
-        # do not add self-loops
-        if edge not in self.adjacency_list and weight > 0.0:
-            self.adjacency_list.append(edge)
+    def _get_edge(self, edge):
+        """ Return reference to Edge() object or None. """
+        for candidate in self._adjacency_list:
+            if candidate == edge:
+                return candidate
+        return None
+
+    def _get_neighbors(self, vertex):
+        """ Return Edge()s where Vertex() param appears.
+
+        :param vertex: point of origin """
+        return [edge for edge in self._adjacency_list if vertex in edge]
+
+    def _get_nearest_neighbor(self, iterable, vertex):
+        """ Return Vertex() with least weight respective to param.
+
+        :param iterable: adjacency list of Edge()s
+        :param vertex: point of origin"""
+        minimum = 100
+
+        neighbors = self._get_neighbors(vertex)
+        output = []
+        for edge in neighbors:
+            if edge.next_node is not vertex:
+                if not edge.next_node.visited:
+                    output.append(edge)
+            elif edge.prev_node is not vertex:
+                if not edge.prev_node.visited:
+                    output.append(edge)
+        output = sorted(output, key=lambda x: x.weight)
+
+        if output[0].prev_node == vertex:
+            neighbor = output[0].next_node
+        else:
+            neighbor = output[0].prev_node
+        return neighbor
 
     def add_vertex(self, vertex):
-        """ Add vertex to Set() of Vertices. """
-        self.vertices.add(vertex)
+        """ Add vertex to Set() of Vertices.
+
+        :param vertex: Vertex() object """
+        self._vertices.add(vertex)
 
     def get_vertex(self, vertex):
-        """ Return reference to vertex. """
-        for item in list(self.vertices):
-            if item == vertex:
-                return item
+        """ Return reference to vertex.
+
+        :param vertex: Vertex() object w/ same address & zip """
+        for candidate in list(self._vertices):
+            if candidate == vertex:
+                return candidate
         return None
 
     def generate_edges(self, index_source, weight_source):
@@ -85,35 +133,18 @@ class Graph:
         :param index_source: ordered List() of Vertex() names & zips
         :param weight_source: ordered 2D-List() of Vertex() edge weights
         """
-        self.indices = [line for line in index_source]
+        self._indices = [line for line in index_source]
         for index, line in enumerate(index_source):
-            self.indices[index] = line
+            self._indices[index] = line
 
-        self.weights = [[]] * len(weight_source)
+        self._weights = [[]] * len(weight_source)
         for index, line in enumerate(weight_source):
-            self.weights[index] = [float(i) for i in line.split('\t')]
+            self._weights[index] = [float(i) for i in line.split('\t')]
 
         # is this possible non-quadratically?
         i = 0
-        while i < len(self.vertices):
-            tmp = list(self.vertices)
-            for loc in self.vertices:
+        while i < len(self._vertices):
+            tmp = list(self._vertices)
+            for loc in self._vertices:
                 self._add_edge(tmp[i], loc)
             i += 1
-
-
-class Edge:
-    # in lieu of traditional Node: node w/ 2 pointers
-    # use 2 Nodes with 1 edge between them
-    def __init__(self, vertex1, vertex2, weight):
-        self.weight = weight
-        self.prev_node = vertex1
-        self.next_node = vertex2
-
-    def __eq__(self, other):
-        return (self.weight == other.weight and
-                (self.prev_node == other.prev_node and self.next_node == other.next_node) or
-                (self.prev_node == other.next_node and self.next_node == other.prev_node))
-
-    def __hash__(self):
-        return hash(str(self))
