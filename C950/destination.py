337f3102633f2b67c5f7bba0ee60b8d3a30a864a
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-11-07 12:43:52 -0600

    refactor: move method to Graph
    move get_nearest_neighbor implementation to Graph class

--- C950/destination.py
+++ C950/destination.py
@@ -27,14 +27,14 @@ class Destination(Vertex):
     def __str__(self):
         return self.address + "\t" + self.city + "\t" + self.state + "\t" + self.zip
 
+    def __lt__(self, other):
+        return (self.address < other.address and
+                self.zip == other.zip)
+
     def __eq__(self, other):
         if other is not None:
             return (self.address == other.address and
                     self.zip == other.zip)
-
-    def __lt__(self, other):
-        return (self.address < other.address and
-                self.zip == other.zip)
 
     def __hash__(self):
         return hash(str(self))
@@ -43,10 +43,3 @@ class Destination(Vertex):
         """ Append package to vertex's list of packages & sort list by deadline. """
         self.packages.append(package)
         self.packages.sort()
-
-    def get_nearest_neighbor(self):
-        """ Return Tuple() of least weighted edge. """
-        min_key = min(self.adjacent, key=self.adjacent.get)
-
-        # use min/max to order vertices for set() comparison
-        return max(self, min_key), min(self, min_key), self.get_weight(min_key)
