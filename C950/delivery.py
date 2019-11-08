ba243eaedae66f1f8a76ed2427d271c878e17364
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-11-07 19:37:50 -0600

    bugs: fix timing issue
    delivery time was being recorded 1 entry late

--- C950/delivery.py
+++ C950/delivery.py
@@ -4,6 +4,8 @@
 # Student ID#: 000917613
 # Mentor: Rebekah McBride
 
+from datetime import timedelta
+
 
 class DeliveryController:
     def __init__(self, route):
@@ -14,37 +16,39 @@ class DeliveryController:
         """
         self._route = route
         self._current_edge = self._route.order[0]
-        self._next_edge = self._current_edge
         self.event_time = self._route.get_start_time()
-        self.wait_time = self._current_edge.weight / self._route._rate_of_travel
         self.total_mileage = 0
 
     def start_route(self):
         """ Initialize variables & advance next_node reference to Destination(). """
+        # set all packages in truck as OUT_FOR_DELIVERY
         for edge in self._route.order:
             node = edge.next_node
             for package in node.packages:
                 package.ofd()
-        self._current_edge = self._next_edge
-        self._next_edge = self._route.get_next_edge()
-        self.event_time = self._route.get_start_time()
-        self.wait_time = self._current_edge.weight / self._route._rate_of_travel
-        self.total_mileage += self._current_edge.weight
 
     def make_delivery(self, current_time):
         """ Mark packages as DELIVERED, set current_node, advance next_node.
 
         :param current_time: datetime of time of delivery
         """
-        # if returning to the beginning, mark route as finished
-        if self._route.get_next_node() == self._route._starting_vertex:
-            self._route.finished = True
-
-        # deliver packages at current address and advance
-        for package in self._route.get_current_node().packages:
-            package.deliver(current_time)
-        self.event_time = current_time
-        self._current_edge = self._next_edge
-        self._next_edge = self._route.get_next_edge()
-        self.wait_time = self._current_edge.weight / self._route._rate_of_travel
+        # advance mileage to destination
         self.total_mileage += self._current_edge.weight
+
+        # update time of next delivery event
+        self.event_time = current_time + timedelta(hours=self._current_edge.weight / self._route.rate_of_travel)
+
+        print("({:5.5} to {:5.5}) : {}/{}miles".format(str(self._current_edge.prev_node),
+                                                       str(self._current_edge.next_node), self._current_edge.weight,
+                                                       self.total_mileage))
+
+        # mark packages at vertex as delivered
+        for package in self._route.get_next_node().packages:
+            package.deliver(self.event_time)
+
+        # advance to next edge
+        try:
+            self._current_edge = self._route.get_next_edge()
+        except AttributeError:
+            self.total_mileage -= self._current_edge.weight
+            self._route.finish_route()
