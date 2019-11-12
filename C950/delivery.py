f980bf9f91307c6d93b6209bf550d4c00ab8bc65
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-11-11 23:56:53 -0600

    Update delivery.py

--- C950/delivery.py
+++ C950/delivery.py
@@ -1,8 +1,7 @@
+# Jeremy Greenwood ----- ID#: 000917613
+# Mentor: Rebekah McBride
 # WGU C950 - Data Structures and Algorithms II
 # Performance Assessment: NHP1
-# Jeremy Greenwood
-# Student ID#: 000917613
-# Mentor: Rebekah McBride
 
 from datetime import timedelta
 
@@ -19,35 +18,37 @@ class DeliveryController:
         self.event_time = self._route.get_start_time()
         self.total_mileage = 0
 
-    def start_route(self):
+    def start_route(self, table):
         """ Initialize variables & advance next_node reference to Destination(). """
         # set all packages in truck as OUT_FOR_DELIVERY
         for edge in self._route.order:
             node = edge.next_node
-            for package in node.packages:
+            for pkg in node.get_package_keys():
+                package = table.search(pkg)
                 package.ofd()
         # mark time for first delivery to execute
-        self.event_time += timedelta(hours=self._current_edge.weight / self._route.rate_of_travel)
+        self.event_time += timedelta(hours=float(self._current_edge.weight) / self._route.rate_of_travel)
 
-    def make_delivery(self, current_time):
+    def make_delivery(self, current_time, table):
         """ Mark packages as DELIVERED, set current_node, advance next_node.
 
         :param current_time: datetime of time of delivery
         """
         # advance mileage to destination
-        self.total_mileage += self._current_edge.weight
+        self.total_mileage += float(self._current_edge.weight)
 
         # mark packages at vertex as delivered
-        for package in self._route.get_next_node().packages:
-            package.deliver(self.event_time)
+        for package in self._route.get_next_node().get_package_keys():
+            pkg = table.search(package)
+            pkg.deliver(self.event_time)
 
         # advance to next edge
         self._current_edge = self._route.get_next_edge()
 
         # update time of next delivery event
-        self.event_time = current_time + timedelta(hours=self._current_edge.weight / self._route.rate_of_travel)
+        self.event_time = current_time + timedelta(hours=float(self._current_edge.weight) / self._route.rate_of_travel)
 
         # if route is complete, mark finished
         if self._current_edge == self._route.order[len(self._route.order) - 1]:
-            self.total_mileage += self._current_edge.weight
+            self.total_mileage += float(self._current_edge.weight)
             self._route.finish_route()
