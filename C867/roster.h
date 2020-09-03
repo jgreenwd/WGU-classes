e31da9c8dcde905b18c0f33f32966ebb58593093
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2020-09-02 20:25:31 -0500

    Update roster.h: swap student data array w/ file read

--- C867/roster.h
+++ C867/roster.h
@@ -14,44 +14,36 @@
 #include "softwareStudent.h"
 
 /* -------- A -------- */
-const string studentData[] = {
-    "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-    "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
-    "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-    "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-    "A5,Jeremy,Greenwood,jgre369@wgu.edu,41,30,30,30,SOFTWARE"
-};
-
 class Roster {
-    private:
-        int lastElementIndex_ = 0;
+private:
+    int lastElementIndex_ = 0;
     
-        /* -------- E.1 -------- */
-        Student* classRosterArray_[];
+    /* -------- E.1 -------- */
+    Student* classRosterArray_[];
     
-    public:
-        Roster();
-        ~Roster();
+public:
+    Roster();
+    ~Roster();
     
-        /* -------- E.3.a -------- */
-        void add(string, string, string, string, int, int, int, int, Degree);
+    /* -------- E.3.a -------- */
+    void add(string, string, string, string, int, int, int, int, Degree);
     
-        /* -------- E.3.b -------- */
-        void remove(const string studentID);
+    /* -------- E.3.b -------- */
+    void remove(const string studentID);
     
-        /* -------- E.3.c -------- */
-        void printAll();
+    /* -------- E.3.c -------- */
+    void printAll();
     
-        /* -------- E.3.d -------- */
-        void printAverageDaysInCourse(string studentID);
+    /* -------- E.3.d -------- */
+    void printAverageDaysInCourse(string studentID);
     
-        /* -------- E.3.e -------- */
-        void printInvalidEmails();
+    /* -------- E.3.e -------- */
+    void printInvalidEmails();
     
-        /* -------- E.3.f -------- */
-        void printByDegreeProgram(Degree);
+    /* -------- E.3.f -------- */
+    void printByDegreeProgram(Degree);
     
-        int size();
+    int size();
 };
 
 #endif /* roster_h */
