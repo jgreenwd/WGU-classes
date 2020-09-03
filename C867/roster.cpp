362c58ee0404108a153554d344da6905d23b88f4
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2020-09-02 20:27:09 -0500

    Update roster.cpp: extract main() to separate file

--- C867/roster.cpp
+++ C867/roster.cpp
@@ -72,7 +72,7 @@ void Roster::printInvalidEmails() {
         
         std::regex entry("\\w+@[a-zA-Z_]+\\.");
         std::smatch match;
-            
+        
         if (!std::regex_search(email, match, entry)) {
             std::cout << email << std::endl;
             allAddressesValid = false;
@@ -94,75 +94,3 @@ void Roster::printByDegreeProgram(Degree degreeProgram) {
 int Roster::size() {
     return lastElementIndex_;
 }
-
-
-/* ----------------- Local functions for parsing input array ----------------- */
-int ishift(std::smatch &sm, std::sregex_iterator iter) {
-    sm = *iter;
-    return stoi(sm.str());
-};
-
-Degree dshift(std::smatch &sm, std::sregex_iterator iter) {
-    sm = *iter;
-    string smVal = sm.str();
-    
-    if (smVal == "SECURITY") return SECURITY;
-    if (smVal == "NETWORK") return NETWORK;
-    if (smVal == "SOFTWARE") return SOFTWARE;
-    return SOFTWARE;
-};
-
-string sshift(std::smatch &sm, std::sregex_iterator iter) {
-    sm = *iter;
-    return sm.str();
-};
-
-
-///* ---------------- F.x ---------------- */
-
-int main() {
-  /*   -------- F.1 -------- */
-    std::cout << "C867 - Scripting and Programming - Applications" << std::endl;
-    std::cout << "C++ - Student ID#: 000917613 - Jeremy Greenwood" << std::endl << std::endl;
-
-  /*   -------- F.2 -------- */
-    Roster classRoster;
-
-    for(auto i: studentData) {
-        string temp;
-        std::regex entry("[^,]+");
-        std::sregex_iterator iter(i.begin(), i.end(), entry), end;
-        std::smatch match = *iter++;
-    
-        string studentID = match.str();
-        string firstName = sshift(match, iter++);
-        string lastName = sshift(match, iter++);
-        string emailAddress = sshift(match, iter++);
-        int age = ishift(match, iter++);
-        int daysInCourse1 = ishift(match, iter++);
-        int daysInCourse2 = ishift(match, iter++);
-        int daysInCourse3 = ishift(match, iter++);
-        Degree degreeProgram = dshift(match,iter++);
-        
-        classRoster.add(studentID, firstName, lastName, emailAddress, age, daysInCourse1, daysInCourse2, daysInCourse3, degreeProgram);
-    }
-
-  /*   -------- F.4 -------- */
-    classRoster.printAll();
-    classRoster.printInvalidEmails();
-    std::cout << std::endl;
-
-    for(int i = 0; i < classRoster.size(); i++) {
-        classRoster.printAverageDaysInCourse("A" + std::to_string(i+1));
-    }
-    std::cout << std::endl;
-
-    classRoster.printByDegreeProgram(SOFTWARE);
-
-    classRoster.remove("A3");
-    classRoster.remove("A3");
-
-    classRoster.~Roster();
-
-    return 0;
-};
